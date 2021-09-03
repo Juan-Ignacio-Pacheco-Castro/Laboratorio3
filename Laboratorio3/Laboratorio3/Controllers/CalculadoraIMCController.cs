60ea8dcc2fe06f9ecd244a87f2b186805ac41dc4
Juan-Ignacio-Pacheco-Castro <juan.pachecocastro@ucr.ac.cr>
2021-09-03 12:18:56 -0600

    Se cambio la L por una i mayuscula debido a confusion

--- Laboratorio3/Laboratorio3/Controllers/CalculadoraIMCController.cs
+++ Laboratorio3/Laboratorio3/Controllers/CalculadoraIMCController.cs
@@ -3,15 +3,21 @@ using Laboratorio3.Models;
 
 namespace Laboratorio3.Controllers
 {
-    public class CalculadoralMCController : Controller
+    public class CalculadoraIMCController : Controller
     {
 
-        public ActionResult ResultadolMC()
+        public ActionResult ResultadoIMC()
         {
             PersonaModel persona = new PersonaModel(1, "Cristiano Ronaldo", 84.0, 1.87);
             double IMC = persona.Peso / (persona.Estatura * persona.Estatura);
             ViewBag.IMC = IMC;
             ViewBag.persona = persona;
+            return View();
+        }
+
+        public ActionResult ResultadosAleatoriosIMC()
+        {
+
             return View();
         }
     }
